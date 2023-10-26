e919c6bdc6ca1c2f334b0743b8ca616d665f8af3
winh321 <69839416+winh321@users.noreply.github.com>
2023-10-26 10:18:18 +0630

    Update note 1.php

--- note 1.php
+++ note 1.php
@@ -2,3 +2,12 @@ route
 Route::get('/user/{id?}/post/{article_name?}',function($id = null,$article_name = "no title"){
     return "$article_name is of $id";
 })->whereNumber('id')->whereAlpha("article_name");
+
+to route
+Route::get('/product',function(){
+    return "This is product page";
+})->name("product");
+
+Route::get('/search',function(){
+    return to_route("product");
+});
